bab5552d40218638cb5df28b072f8a75bbb45be8
gq3abc <gq3abc@gmail.com>
2022-07-29 17:29:55 +0200

    update

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,6 @@
-var theme = 'auto';
+var theme = localStorage.getItem('theme');
 
+if(theme == null){ theme = 'auto'; }
 
 /*var loc = window.location.pathname;
 var dir = loc.substring(0, loc.lastIndexOf('/'));
@@ -7,6 +8,8 @@ dircount = (dir.match(new RegExp("/", "g")) || []).length;
 let text = "../";
 let d = text.repeat(dircount);
 //alert(d);*/
+
+
 
 function themeAuto(){
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -16,9 +19,35 @@ document.getElementById('theme').href = '/css/light.css';
 }
 }
 
-if(theme == 'auto'){
+function setTheme(mode){
+
+switch (mode) {
+
+case 'light':
+document.getElementById('theme').href = '/css/light.css';
+break;
+
+case 'dark':
+document.getElementById('theme').href = '/css/dark.css';
+break;
+
+case 'random':
+let themelist = [
+"/css/light.css",
+"/css/dark.css"
+];
+
+document.getElementById('theme').href = themelist[Math.floor(Math.random()*themelist.length)];
+break;
+
+default:
+
 themeAuto();
 window.matchMedia("(prefers-color-scheme: light)").addEventListener("change", function () {
 themeAuto();
 });
+
 }
+}
+
+setTheme(theme);
