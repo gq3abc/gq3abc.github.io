9e00733169887b81211fe78e26d6f7389a01d28a
gq3abc <gq3abc@gmail.com>
2022-07-30 20:54:52 +0200

    update

--- js/theme.js
+++ js/theme.js
@@ -2,13 +2,6 @@ var theme = localStorage.getItem('theme');
 var themeList = ["light", "dark", "light-blue", "light-green", "light-orange"];
 if(theme == null||theme == ''){ theme = 'auto'; }
 
-/*var loc = window.location.pathname;
-var dir = loc.substring(0, loc.lastIndexOf('/'));
-dircount = (dir.match(new RegExp("/", "g")) || []).length;
-let text = "../";
-let d = text.repeat(dircount);
-//alert(d);*/
-
 
 
 function themeAuto(){
@@ -24,8 +17,6 @@ function setTheme(mode){
 themeList.forEach((element) => {
 if(mode == element){ document.getElementById('theme').href = '/css/'+mode+'.css'; }
 })
-
-
 
 
 switch (mode) {
